d0f691d908cfa08387b19bfbcc9c54179bf305a4
sinan <210928@tkmce.ac.in>
2023-12-07 02:20:40 +0530

    added delete feature

--- src/Components/StaffDiv/StaffDiv.jsx
+++ src/Components/StaffDiv/StaffDiv.jsx
@@ -1,31 +1,58 @@
-import React,{useState,useEffect} from 'react';
-import './StaffDiv.css'; 
+import React, { useState, useEffect } from 'react';
+import './StaffDiv.css';
 import axios from '../../axios';
 
 const StaffDiv = () => {
   const [staffs, setStaffs] = useState([]);
+  const [selectedStaffId, setSelectedStaffId] = useState(null);
 
   useEffect(() => {
-    axios.get('/staffs') 
-      .then(response => {
+    axios.get('/staffs')
+      .then((response) => {
         console.log(response.data);
-        setStaffs(response.data); 
+        setStaffs(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
- 
+
+  const handleStaffDelete = async (staffId) => {
+    try {
+      await axios.delete(`/staffs/${staffId}`);
+      setStaffs((staffs) => staffs.filter((staff) => staff.Staff_id !== staffId));
+      setSelectedStaffId(null);
+    } catch (error) {
+      console.error('Error deleting staff:', error);
+    }
+  };
+
+  const ConfirmationModal = () => (
+    selectedStaffId && (
+      <div className="confirm-modal">
+        <p>Are you sure you want to delete Staff {selectedStaffId}?</p>
+        <button onClick={() => handleStaffDelete(selectedStaffId)}>
+          Yes, Delete
+        </button>
+        <button onClick={() => setSelectedStaffId(null)}>Cancel</button>
+      </div>
+    )
+  );
+
   return (
-    <div className='staffdiv'>
-      {staffs.map(e=>(
-        <div className='staffdiv-child'>
-        <p>{e.Staff_id}</p>
-        <p>{e.Name}</p>
-        <p>{e.Type}</p>
-        <p>Rs {e.Salary}</p>
-     </div>
+    <div className="staffdiv">
+      {staffs.map((staff) => (
+        <div key={staff.Staff_id} className="staffdiv-child">
+          <p>{staff.Staff_id}</p>
+          <p>{staff.Name}</p>
+          <p>{staff.Type}</p>
+          <p>Rs. {staff.Salary}</p>
+          <button onClick={() => setSelectedStaffId(staff.Staff_id)}>
+            Delete
+          </button>
+        </div>
       ))}
+      <ConfirmationModal />
     </div>
   );
 };
--- src/Components/StudentDiv/StudentDiv.jsx
+++ src/Components/StudentDiv/StudentDiv.jsx
@@ -1,34 +1,61 @@
-import React,{useEffect,useState} from 'react'
-import './StudentDiv.css'
-import axios from '../../axios'
+import React, { useEffect, useState } from 'react';
+import axios from '../../axios';
+import './StudentDiv.css';
 
 const StudentDiv = () => {
-    const [students, setStudents] = useState([]);
+  const [students, setStudents] = useState([]);
+  const [selectedStudentId, setSelectedStudentId] = useState(null);
 
   useEffect(() => {
-    axios.get('/students') 
-      .then(response => {
+    axios.get('/students')
+      .then((response) => {
         console.log(response.data);
-        setStudents(response.data); 
+        setStudents(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
 
-  return (
-    <div className='studentdiv'>
-    {students.map(e=>(
-        <div key={e.Student_id} className='studentdiv-child'>
-        <p>{e.Student_id}</p>
-        <p>{e.Name}</p>
-        <p>{e.Room_id}</p>
-        <p>{e.Place}</p>
-        <p>{e.Phone_no}</p>
-     </div>
-    ))}
-    </div>
-  )
-}
+  const handleStudentDelete = async (studentId) => {
+    try {
+      await axios.delete(`/students/${studentId}`);
+      setStudents((students) => students.filter((student) => student.Student_id !== studentId));
+      setSelectedStudentId(null);
+    } catch (error) {
+      console.error('Error deleting student:', error);
+    }
+  };
 
-export default StudentDiv
+  const ConfirmationModal = () => (
+    selectedStudentId && (
+      <div className="confirm-modal">
+        <p>Are you sure you want to delete Student {selectedStudentId}?</p>
+        <button onClick={() => handleStudentDelete(selectedStudentId)}>
+          Yes, Delete
+        </button>
+        <button onClick={() => setSelectedStudentId(null)}>Cancel</button>
+      </div>
+    )
+  );
+
+  return (
+    <div className="studentdiv">
+      {students.map((student) => (
+        <div key={student.Student_id} className="studentdiv-child">
+          <p>{student.Student_id}</p>
+          <p>{student.Name}</p>
+          <p>{student.Room_id}</p>
+          <p>{student.Place}</p>
+          <p>{student.Phone_no}</p>
+          <button onClick={() => setSelectedStudentId(student.Student_id)}>
+            Delete
+          </button>
+        </div>
+      ))}
+      <ConfirmationModal />
+    </div>
+  );
+};
+
+export default StudentDiv;
